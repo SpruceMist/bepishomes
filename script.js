1019e438992f67132138aadcc633b0b0b95e91b0
SpruceMist <sprucemist25986@gmail.com>
2019-11-14 10:45:50 -0600

    fixed links

--- script.js
+++ script.js
@@ -147,9 +147,9 @@ document.getElementById("pic_12").addEventListener("click", function() {
 var images = ["pictures/img-1.jpg","pictures/img-2.jpg"];
 
 function display_next_image_1() {
-    if ("https://sprucemist.github.io/bepishomes/pictures/img-1.jpg" == document.getElementById("image-1").src) {
-        document.getElementById("image-1").src = "https://sprucemist.github.io/bepishomes/pictures/img-11.jpg";
+    if ("sprucemist.github.io/bepishomes/pictures/img-1.jpg" == document.getElementById("image-1").src) {
+        document.getElementById("image-1").src = "sprucemist.github.io/bepishomes/pictures/img-11.jpg";
     } else {
-        document.getElementById("image-1").src = "https://sprucemist.github.io/bepishomes/pictures/img-1.jpg";
+        document.getElementById("image-1").src = "sprucemist.github.io/bepishomes/pictures/img-1.jpg";
     }
 }
